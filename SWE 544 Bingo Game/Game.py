ba3638a101edf94d60975c20c9029ef814c7df31
TalatCikikci <talat.cikikci@gmail.com>
2016-01-11 22:47:04 +0200

    Major Implementations in Game.py
    Added game procedures to the game.py

--- SWE 544 Bingo Game/Game.py
+++ SWE 544 Bingo Game/Game.py
@@ -1,5 +1,5 @@
-import BingoCardGenerator
 import threading
+import random
 
 class GameSession (threading.Thread):
 	
@@ -8,6 +8,8 @@ class GameSession (threading.Thread):
 		self.userList = userList
 		self.id = id
 		self.userGameArray = []
+		self.userUpdatedArray = []
+		self.drawnNumbers = []
 		
 	def prepareSession(self):
 		existingCards = range(len(self.userList))
@@ -16,7 +18,7 @@ class GameSession (threading.Thread):
 		for user in self.userList:
 			singleUser = range(2)
 			while True:
-				singleUser[0], singleUser[1] = BingoCardGenerator.generateBingoCard()
+				singleUser[0], singleUser[1] = self.generateBingoCard()
 				if singleUser[1] in existingCards:
 					continue
 				else:
@@ -26,6 +28,116 @@ class GameSession (threading.Thread):
 					sessionUserSlot += 1
 					break
 
+	def drawRandomNumber(self):
+		while True:
+			drawnNumber = random.randint(1,90)
+			if drawnNumber in self.drawnNumbers:
+				continue
+			else:
+				self.drawnNumbers.append(drawnNumber)
+				break
+		return drawnNumber
+		
+	def checkCardsForDrawn(self, checkNumber):
+		for i in range(len(self.userList)):
+			if self.userGameArray[0] == i:
+				tempCard = self.userGameArray[1]
+				tempList = tempCard[1]
+				for row in range(len(tempList)):
+					if checkNumber in tempList[row]:
+						tempRow = tempList[row]
+						theIndex = tempRow.index(checkNumber)
+						tempRow[theIndex] = 'X'
+					tempList[row] = tempRow
+				tempCard[1] = tempList
+				self.userUpdatedArray[1] = tempCard
+				
+	def generateBingoCard(self):
+
+	# Will hold the generated bingo card tuples.
+	bingoCard = []
+	# Holds the already selected numbers.
+	cardChecklist = []
+
+	# Loop for 3 rows.
+	for cinquina in range(3):
+
+		# Column iterator.
+		i = 0
+		# Holds numbers for the current row.
+		cinquinaAccepted = []
+
+		while i < 5:
+			# Generate random number.
+			cardCandidate = random.randint(1,90)
+			
+			# Check if number already exists in the card.
+			if cardCandidate in cardChecklist:
+				continue
+				
+			# Insert initial number to the card.
+			elif len(cinquinaAccepted) == 0:
+				cardChecklist.append(cardCandidate)
+				cinquinaAccepted.append(cardCandidate)
+				i += 1
+			
+			else:
+				for alreadyIncluded in cinquinaAccepted:
+					
+					# Check if the number exists in the current row.
+					if cardCandidate in cinquinaAccepted:
+						continue
+					
+					alreadyIncludedStr = str(alreadyIncluded)
+					cardCandidateStr = str(cardCandidate)
+					
+					# A row must have at most one of each tenths digit. Check for "0" tenths.
+					if len(alreadyIncludedStr) == 1:
+						if alreadyIncludedStr == cardCandidateStr:
+							continue
+						else:
+							cardChecklist.append(cardCandidate)
+							cinquinaAccepted.append(cardCandidate)
+							i += 1
+						
+					# A row must have at most one of each tenths digit. Check for tenths digit > "0"
+					else:
+						if alreadyIncludedStr[0] == cardCandidateStr[0]: 
+							continue
+						else:
+							cardChecklist.append(cardCandidate)
+							cinquinaAccepted.append(cardCandidate)
+							i += 1
+					
+					continue
+		
+		# Sort row in ascending order
+		cinquinaAccepted = sorted(cinquinaAccepted)
+		bingoCard.append(cinquinaAccepted)
+
+	# This commented section is for debugging.
+	# for i in range(3):
+		# print(bingoCard[i])
+	
+	cardChecklist = sorted(cardChecklist)
+	
+	return bingoCard, cardChecklist
+				
 	def run(self):
 		self.prepareSession()
-		print(self.userGameArray)
+		# Commented out print for testing purposes.
+		# print(self.userGameArray)
+		
+		self.userUpdatedArray = self.userGameArray
+		
+		while True:
+			okCount = 0
+			theNumber = self.drawRandomNumber()
+			checkCardsForDrawn(theNumber)
+			
+			###### SEND NR TO ALL PAYERS
+			
+			while okCount < len(self.userList)
+				
+				###### WAIT FOR OK FROM PLAYERS
+		
